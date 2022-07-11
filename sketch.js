b2cd1de33c073a3aa3c98f778e3089616a8754f1
Ayush2000005 <109070304+Ayush2000005@users.noreply.github.com>
2022-07-11 14:42:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var box;
+function setup() {
+  createCanvas(500,500);
+  box=createSprite(200,300,30,30)
+  box.shapeColor="red"
+}
+
+function draw() 
+{
+  background("white");
+if(keyIsDown(RIGHT_ARROW)){
+box.position.x=box.position.x+5
+}
+if(keyIsDown(LEFT_ARROW)){
+box.position.x=box.position.x-5
+}
+if(keyIsDown(UP_ARROW)){
+box.position.y=box.position.y-5
+}
+if(keyIsDown(DOWN_ARROW)){
+box.position.y=box.position.y+5
+}
+  drawSprites()
+}
+
+
+
+
